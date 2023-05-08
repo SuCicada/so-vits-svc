ffd71fb094c36d4c937d1ab5f9770a909a9815af
pengyifu <pengyifu@gmail.com>
2023-05-08 12:26:27 +0900

    vits_gradio.py

--- tools/vits_gradio.py
+++ tools/vits_gradio.py
@@ -19,18 +19,17 @@ class VitsGradio:
 
     def __init__(self):
         with gr.Blocks() as self.Vits:
-            gr.Markdown("## issue and source in: [SuCicada/SuTTS](https://github.com/SuCicada/SuTTS) ")
+            gr.Markdown("# Lain TTS with so-vits-svc-4.0 ")
+            gr.Markdown("## web source in: [SuCicada/so-vits-svc](https://github.com/SuCicada/so-vits-svc/blob/4.0/tools/vits_gradio.py) ")
+            gr.Markdown("## Models in: [SuCicada/Lain-so-vits-svc-4.0](https://huggingface.co/SuCicada/Lain-so-vits-svc-4.0/tree/main) ")
 
             with gr.Tabs():
                 with gr.TabItem("tts"):
                     with gr.Row() as self.VoiceConversion:
-                        self.text = gr.Textbox(label="输入文本", lines=5,
+                        self.text = gr.Textbox(label="input text", lines=5,
                                                value="こんにちは、私は人工知能です。私は、あなたの声を聞くことができます。")
                     with gr.Row():
                         with gr.Column():
-                            # with gr.Row():
-                            #     with gr.Column():
-                            # self.srcaudio = gr.Audio(label = "输入音频")
                             with gr.Row():
                                 self.tts_engine = gr.inputs.Dropdown(["edge-tts", "gtts", ], default="edge-tts",
                                                                      label="tts engine")
@@ -39,15 +38,15 @@ class VitsGradio:
                                 self.speed = gr.inputs.Slider(minimum=0, maximum=2, step=0.1, default=1,
                                                               label="speed")
 
+                            self.tts_submit = gr.Button("Transform")
                         with gr.Column():
                             self.origin_output = gr.Audio(label="origin voice")
-                            self.tts_submit = gr.Button("Transform")
                     self.tts_target_output: gr.Audio = gr.Audio(label="lain voice")
 
                 with gr.TabItem("svc"):
-                    with gr.TabItem("音频转音频"):
-                        self.svc_input_audio = gr.Audio(label="选择音频")
-                        self.vc_submit = gr.Button("音频转换", variant="primary")
+                    with gr.TabItem("voice to voice"):
+                        self.svc_input_audio = gr.Audio(label="choose audio file")
+                        self.vc_submit = gr.Button("voice conversion", variant="primary")
 
                     self.svc_target_output: gr.Audio = gr.Audio(label="lain voice")
 
